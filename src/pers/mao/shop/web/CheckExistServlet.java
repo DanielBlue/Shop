3402e9b340870d27e6a1d15631bd8a1a60118e1b
Maoqi <eita.xbx@gmail.com>
2017-12-19 22:13:18 +0800

    update

--- src/pers/mao/shop/web/CheckExistServlet.java
+++ src/pers/mao/shop/web/CheckExistServlet.java
@@ -1,5 +1,7 @@
 package pers.mao.shop.web;
 
+import pers.mao.shop.service.UserService;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,7 +15,10 @@ public class CheckExistServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, IOException {
         String username = request.getParameter("username");
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+        UserService service = new UserService();
+        boolean userExisted = service.isUserExisted(username);
+        String json = "{\"isExist\":"+userExisted+"}";
+        response.getWriter().write(json);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
